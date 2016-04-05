ad87cbeea583253b4eb0ba1fcbe2242bc6832b72
kanekotic <alvarojosepl@gmail.com>
2016-04-05 08:28:29 +0200

    added unit tests and fix issues

---
+++ test/post-test.js
@@ -0,0 +1,47 @@
+var chai = require('chai');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+var post = require('../lib/post');
+
+/*expect(hello).to.not.equal('Hello');
+expect(hello).to.be.a('string');
+expect(hello).to.not.be.a('number');
+expect(hello).to.contain('he');*/
+
+var before = function(){
+    date = new Date(2011, 0, 1, 2, 3, 4, 567);
+    origin = "Twitter";
+    owner = "Jhon";
+    content = "Loren Ipsum Troloro";
+};
+
+var testCorrectDate = function(){
+    var localPost = new post(date, origin, owner, content);
+    expect(localPost.date).to.equal(date);
+};
+
+var testCorrectOrigin = function(){
+    var localPost = new post(date, origin, owner, content);
+    expect(localPost.origin).to.equal(origin);
+};
+
+var testCorrectOwner = function(){
+    var localPost = new post(date, origin, owner, content);
+    expect(localPost.owner).to.equal(owner);
+};
+
+var testCorrectContent = function(){
+    var localPost = new post(date, origin, owner, content);
+    expect(localPost.content).to.equal(content);
+};
+
+describe('Post Initialization', function(){
+    before(before);
+    it ('returns correct date', testCorrectDate);
+    it ('returns correct origin', testCorrectOrigin);
+    it ('returns correct owner', testCorrectOwner);
+    it ('returns correct content', testCorrectContent);
+});
